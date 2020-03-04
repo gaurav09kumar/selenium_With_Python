1da4644dfe291d2f61d6ba9d58fc39a23ec0286b
kumargaurav09 <kumargaurav09@outlook.com>
2020-03-04 21:22:20 +0530

    This is new upload

---
+++ MultiBrowser_Prg1.py
@@ -0,0 +1,27 @@
+# Visit www.seleniumhq.org to download the required drivers
+
+# This program displays - How to Run Tests on Chrome, Firefox & IE Browsers
+# import  web driver from selenium package into the program
+
+from selenium import webdriver
+
+# set the path for chrome
+# (For IE & Firefox replace webdriver.Chrome with webdriver.IE & webdriver.Firefox, change executable path accordingly)
+driver = webdriver.Chrome(executable_path="C:/Users/kumar/Downloads/chromedriver_win32(1)/chromedriver.exe")
+
+# enter the URL to open the page in browser
+driver.get("https://www.facebook.com")
+
+# get title of page
+print("Title of the page is : "+driver.title)
+
+# to return the URL's of the web page
+print(driver.current_url)
+
+# to get the HTML Source Code of the web page
+print(driver.page_source)
+
+# close the browser
+driver.close()
+
+
